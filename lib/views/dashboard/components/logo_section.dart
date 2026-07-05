88d9a75ea0ecc80ac935d0ba2e6ddbc05809e3bd
donc-py <bitpattern0@gmail.com>
2026-07-04 23:26:42 -0500

    fix logo section to home

--- lib/views/dashboard/components/logo_section.dart
+++ lib/views/dashboard/components/logo_section.dart
@@ -1,36 +1,28 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:readypos_flutter/gen/assets.gen.dart';
+import 'package:readypos_flutter/routes.dart';
 
 class LogoSection extends StatelessWidget {
-  const LogoSection({
-    super.key,
-  });
+  const LogoSection({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
-          padding: EdgeInsets.only(bottom: 1.w, right: 1.w, top: 1.h),
-          // child: SvgPicture.asset(
-          //   AdaptiveTheme.of(context).mode.isDark
-          //       ? Assets.svgs.logowhite
-          //       : Assets.svgs.logoblack,
-          //   fit: BoxFit.fill,
-          // ),
-          child: AdaptiveTheme.of(context).mode.isDark
-              ? Assets.pngs.logoMusicom.image(
-                  // width: 250.w,
-                  height: 50.h,
-                )
-              : Assets.pngs.logoMusicom.image(
-                  // width: 250.w,
-                  height: 50.h,
-                ),
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => Navigator.pushNamedAndRemoveUntil(
+            context,
+            Routes.core,
+            (_) => false,
+          ),
+          child: Container(
+            padding: EdgeInsets.only(bottom: 1.w, right: 1.w, top: 1.h),
+            child: Assets.pngs.logoMusicom.image(height: 50.h),
+          ),
         ),
       ],
     );
